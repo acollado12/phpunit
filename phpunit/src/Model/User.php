930793fd960c3b8d114708ab923d19951d2280a0
aaron <alugar3293@ieselcaminas.org>
2022-11-28 19:04:25 +0100

    Tercera Subida

--- phpunit/src/Model/User.php
+++ phpunit/src/Model/User.php
@@ -11,4 +11,17 @@ $this->age = $age;
 public function isAnAdult(): bool{
 return $this->age >= 18;
 }
+
+public function testIsAnAdultReturnsTrueForPeopleOrderThanTreshold(){
+    new User(19);
+    $this->assertTrue($user->isAnAdult());
+    
+    $user = new User(55);
+    $this->assertTrue($user->isAnAdult());
+    
+    $user = new User(102);
+    $this->assertTrue($user->isAnAdult());
+    
+    
+}
 }
